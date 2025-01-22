69d5f5b3161c756d4eafab9e785a7fd74a840a7e
Behnaz <behnaz.shojaei88@gmail.com>
2025-01-22 13:16:54 +0800

    community user serializer created, pending resolve conflict with prev codebase to test

--- sheinspires/users/serializers.py
+++ sheinspires/users/serializers.py
@@ -9,3 +9,20 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
+    
+
+class CommunityUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'image', 'current_role', 'location']
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'image': {'required': True},
+            'current_role': {'required': True},
+            'location': {'required': True},
+        }
+
+    def create(self, validated_data):
+        validated_data['user_type'] = CustomUser.USER_TYPES["COMMUNITY_USER"]
+        return CustomUser.objects.create_user(**validated_data)
